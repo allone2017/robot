84fbcb3a1c7a1f66ad00422a39f0dcc5ea2b6ae3
allone2017 <512995370@qq.com>
2017-03-01 12:29:40 +0800

    Update robotstar.php

--- robotstar.php
+++ robotstar.php
@@ -1,3 +1,4 @@
+<?php
 curl 'https://oapi.dingtalk.com/robot/send?access_token=6b29ffe2d5f19b6f9a081010a5b0475ffffa28091c30d97368dfebce199d06e1' \
    -H 'Content-Type: application/json' \
    -d '
@@ -6,3 +7,4 @@ curl 'https://oapi.dingtalk.com/robot/send?access_token=6b29ffe2d5f19b6f9a081010
         "content": "我就是我, 是不一样的烟火"
      }
   }'
+?>
